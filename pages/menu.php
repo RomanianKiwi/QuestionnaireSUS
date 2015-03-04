42f279e5ca6fad9294307f61b3a670e9dbac6dbc
Neykhot <maxime.bediguian@laposte.net>
2015-03-04 15:00:52 +0100

    diverses modifications des pages du site

---
+++ pages/menu.php
@@ -0,0 +1,33 @@
+
+		<nav class="navbar navbar-default" role="navigation">
+					<div class="container">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-menu">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Menu</a>
+						</div>
+
+						<div class="collapse navbar-collapse" id="main-menu">
+							<ul class="nav navbar-nav">
+								<li>
+									<a id ="ajoutQuest" href="AjoutQuestionnaire.php">Nouvelle enquête</a>
+									
+								</li>
+								<li><a href="mailing.php">Consultation résultats</a></li>
+								<li>
+									<a href="consult.php">Invitation participants </a>
+									
+								</li>
+								<li><a href="AjoutAdminEval.php">Ajouter évaluateur</a></li>
+							</ul>
+							<ul class="nav navbar-nav navbar-right">
+								<li><a id="deco" class="dropdown-toggle" >Déconnexion</a></li>
+							</ul>
+						</div> 
+					</div>
+		</nav>
