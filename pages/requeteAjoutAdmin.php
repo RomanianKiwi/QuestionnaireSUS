ac305f64e2c466afa0177f333710adb76f12f0fa
jeremyAncy <bocchi31@gmail.com>
2015-03-08 16:08:28 +0100

    Pour MAXIME mets requetesAjoutAdmin php dans ton dossier et regarde dans le fichier AjoutAdminEval ce que j ai mis dans la partie javaScript

---
+++ pages/requeteAjoutAdmin.php
@@ -0,0 +1,23 @@
+<?php
+
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=projet;charset=utf8', 'root', '');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+$nom = $_POST['NomU'];
+$mdp = $_POST['MdpU'];
+
+$bdd->exec("INSERT INTO administrateur VALUES ('',". $nom ."," . $mdp .")");
+
+/*
+$req = $bdd->prepare("INSERT INTO administrateur VALUES ('',:nom,:mdp);");
+
+$req->execute(array(
+	'nom' => $nom,
+	'mdp' => $mdp
+	));*/
+
+?>
+
