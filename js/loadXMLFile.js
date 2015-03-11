4f9a2d219ba401b31d45e1e3dd16f9c272a85556
RomanianKiwi <ciubotariu.flavien@gmail.com>
2015-03-11 17:35:02 +0100

    #1 traduction des réponses du questionnaire

--- js/loadXMLFile.js
+++ js/loadXMLFile.js
@@ -1,41 +1,42 @@
     function loadXMLDoc(url){
         var xmlhttp;
-        var questions, formGroup, i;
+        var answers, reponses, formGroup, i;
 
         if (window.XMLHttpRequest){
-            // code pour IE7+, Firefox, Chrome, Opera, Safari
+            // code for IE7+, Firefox, Chrome, Opera, Safari
             xmlhttp=new XMLHttpRequest();
         }
         else{
-            // code pour IE6, IE5
+            // code for IE6, IE5
             xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
         }
 
         xmlhttp.onreadystatechange=function(){
-            // on regarde si la requête est terminée et la réponse prête
+			// we check if the query is achieved and the response is ready
             if(xmlhttp.readyState == 4 && xmlhttp.status ==200){
                 $("#questSUS").html("");
                 questions = xmlhttp.responseXML.documentElement.getElementsByTagName("question");
+				answers = xmlhttp.responseXML.documentElement.getElementsByTagName("answer");
                 for(i=0; i<questions.length; i++){
                     var question = questions[i].firstChild.nodeValue;
                     formGroup="<div class='form-group'>"
-                            +"<h4>"+question+"</h4>"
-                            +"<label class='radio-inline'>"
-                                +"<input type='radio' name='question"+i+"' value='5' checked>Tout à fait d'accord"
-                            +"</label>"
-                            +"<label class='radio-inline'>"
-                                +"<input type='radio' name='question"+i+"' value='4'>Plutôt d'accord"
-                            +"</label>"
-                            +"<label class='radio-inline'>"
-                                +"<input type='radio' name='question"+i+"' value='3' checked>Partiellement d'accord"
-                            +"</label>"
-                            +"<label class='radio-inline'>"
-                                +"<input type='radio' name='question"+i+"' value='2'>Un peu d'accord"
-                            +"</label>"
-                            +"<label class='radio-inline'>"
-                                +"<input type='radio' name='question"+i+"' value='1' checked>Pas du tout d'accord"
-                            +"</label>"
-                        +"</div>";
+								+"<h4>"+question+"</h4>"
+								+"<label class='radio-inline'>"
+									+"<input type='radio' name='question"+i+"' value='5'>"+answers[0].firstChild.nodeValue
+								+"</label>"
+								+"<label class='radio-inline'>"
+									+"<input type='radio' name='question"+i+"' value='4'>"+answers[1].firstChild.nodeValue
+								+"</label>"
+								+"<label class='radio-inline'>"
+									+"<input type='radio' name='question"+i+"' value='3'>"+answers[2].firstChild.nodeValue
+								+"</label>"
+								+"<label class='radio-inline'>"
+									+"<input type='radio' name='question"+i+"' value='2'>"+answers[3].firstChild.nodeValue
+								+"</label>"
+								+"<label class='radio-inline'>"
+									+"<input type='radio' name='question"+i+"' value='1'>"+answers[4].firstChild.nodeValue
+								+"</label>"
+							+"</div>";
 
                         $("#questSUS").append(formGroup);
                 }
