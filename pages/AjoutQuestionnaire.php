48051b944fed5346ea7d67886f5a1d34174aec4b
jeremyAncy <bocchi31@gmail.com>
2015-03-16 15:22:37 +0100

    merge entre master et requeteSqlJeremy

--- pages/AjoutQuestionnaire.php
+++ pages/AjoutQuestionnaire.php
@@ -10,6 +10,11 @@
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 		
+		<!-- plugin Bootstrap DatePicker -->
+		<link href="../css/datepicker.css" rel="stylesheet">
+		<script src="../js/bootstrap-datepicker.js"></script>
+		<script src="../js/dateFRtoEN.js"></script>
+		
 		<script type="text/javascript">
 
             $(document).ready(function () {
@@ -27,6 +32,26 @@
                         }
                     });
                 }
+				
+				
+				$('#formAjout').on('submit', function(e) {
+						//ajoutQuestionnaire($("#NomSysteme").val(),$("#dateDebut").val(),2);
+						var date = $("#datepicker").val();
+						//console.log( date);
+						
+						//var date2 = $("#datepicker").val($.datepicker.formatdate('dd-mm-yyyy',new Date()));
+						//console.log( DateFrtoEn(date));
+						
+						ajoutQuestionnaire($("#nomQuestionaire").val(),DateFrtoEn(date),2);
+					});
+				$(function() {
+					$("#datepicker").datepicker({
+						format : 'dd/mm/yyyy' 
+					});
+				});
+				
+
+
 				
 				//ajoutQuestionnaire("GPS", "2015-03-15",2);
                 
@@ -62,21 +87,21 @@
 							<input id="nomQuestionaire" type="text" class="form-control" placeholder="Nom du Questionnaire" required>
 						</div>
 					</div>
-					
+					<!--
 					<div class="form-group">
 						<label for="NomSysteme" class="col-sm-3 control-label">Nom du Systeme</label>
 						<div class="col-sm-4">
 							<input id="NomSysteme" type="text" class="form-control" placeholder="Nom du Systeme a évalué" required>
 						</div>
 					</div>
-					
+					-->
 					<div class="form-group">
 						<label id="dateId" for="date" class="col-sm-3 control-label">Date de création du Questionnaire</label>
 						<div class="col-sm-4">
-							<input id="dateDebut"  class="form-control" placeholder="Date de Debut">
+							<input id="datepicker" class="form-control " placeholder="Date de Debut">
 						</div>
 					</div>
-					
+					<!--
 					<div class="form-group">
 						<label id="dateId" for="date" class="col-sm-3 control-label">Fin de validité du questionnaire</label>
 						<div class="col-sm-4">
@@ -91,7 +116,7 @@
 							<INPUT type="radio" name="formulaireDémo" value="NON">Non<br>
 						</div>
 					</div>
-					
+					-->
 					<div class="form-group">
 						<div class="col-sm-offset-3 col-sm-10">
 							<button id="ajouter" type="submit" class="btn btn-primary">Ajouter</button>
