a3586f7d281292e99f30c4536f4e821cf83ee85e
RomanianKiwi <ciubotariu.flavien@gmail.com>
2015-03-22 12:11:50 +0100

    #9 script de génération de graphique

---
+++ js/generateChart.js
@@ -0,0 +1,59 @@
+    /*
+     * @param nameSystem : the name of the system
+     * @param versions : array containing the differents versions of the system
+     * @apram results : array containing all results of each versions of the 
+     *                  system
+     */
+    function generateChart (nameSystem, versions, results) {
+        $('#chartContainer').highcharts({
+            chart: {
+                type: 'spline'
+            },
+            credits: {
+                enabled: false
+            },
+            title: {
+                text: 'Score SUS de ' + nameSystem + ' au fil de ses versions'
+            },
+            subtitle: {
+                text: 'Source: thesource.com by Gones'
+            },
+            xAxis: {
+                title :{
+                    text: 'Version'
+                },
+                categories: versions
+            },
+            yAxis: {
+                title: {
+                    text: 'Score SUS (pts)'
+                }
+            },
+            tooltip: {
+                valueSuffix : ' pts'
+            },
+            plotOptions: {
+                spline: {
+                    marker: {
+                        radius: 4,
+                        lineColor: '#666666',
+                        lineWidth: 1
+                    }
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: [{
+                name: nameSystem,
+                color: '#FFDA00',
+                marker: {
+                    symbol: 'circle'
+                },
+                data: results
+            }]
+        });
+    }
