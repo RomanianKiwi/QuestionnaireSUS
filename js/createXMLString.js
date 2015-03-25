28805f89126a8502f0de852d61d788c8ef5579b2
jeremyAncy <bocchi31@gmail.com>
2015-03-25 17:08:38 +0100

    generation du code XML ok

--- js/createXMLString.js
+++ js/createXMLString.js
@@ -1,25 +1,23 @@
 
-    /*
-     * @param system : the name of the system
-     * @param answers : array containing the answers of the questionnaire
-     * @apram questions : array containing the questions of the questionnaire
-     * @return : a string of xml content
-     */
-    function createXMLString(system, answers, questions){
-        var i;
-        var xmlString = "<?xml version='1.0' encoding='UTF-8'?>";
-        xmlString = xmlString + "<questionnaire><system>" + system + 
-                    "</system>";
-        xmlString = xmlString + "<answers>";
-        for(i = 0; i < answers.length; i++)
-                xmlString = xmlString + "<answer>" + answers[i] + "</answer>";
-        xmlString = xmlString + "</answers>";
-        xmlString = xmlString + "<questions>";
-        for(i = 0; i < questions.length; i++)
-                xmlString = xmlString + "<question>" + questions[i] + 
-                            "</question>";
-        xmlString = xmlString + "</questions>";
-        xmlString = xmlString + "</questionnaire>";
+/*
+ * @param system : the name of the system
+ * @param answers : array containing the answers of the questionnaire
+ * @apram questions : array containing the questions of the questionnaire
+ * @return : a string of xml content
+ */
+function createXMLString(system, answers, questions) {
+    var i;
+    var xmlString = "<?xml version='1.0' encoding='UTF-8'?>\n";
+    xmlString = xmlString + "<questionnaire>\n    <system>" + system + "</system>";
+    xmlString = xmlString + "\n    <answers>";
+    for (i = 0; i < answers.length; i++)
+        xmlString = xmlString + "\n      <answer>" + answers[i] + "</answer>";
+    xmlString = xmlString + "\n    </answers>";
+    xmlString = xmlString + "\n    <questions>";
+    for (i = 0; i < questions.length; i++)
+        xmlString = xmlString + "\n      <question>" + questions[i] + "</question>";
+    xmlString = xmlString + "\n    </questions>";
+    xmlString = xmlString + "\n</questionnaire>";
 
-        return xmlString;
-    }
+    return xmlString;
+}
