e28ab934873aec3f1dad4aab5ae7d12947c273af
jeremyAncy <bocchi31@gmail.com>
2015-04-01 09:11:14 +0200

    modification mineur entete index php

--- index.php
+++ index.php
@@ -43,9 +43,9 @@
     <head>
         <meta charset="UTF-8">
         <title>Questionnaire SUS</title>
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-        <script src="http://code.jquery.com/jquery-latest.js"></script>
-        <script src="js/bootstrap.js"></script>
+		<link rel="stylesheet" href="/css/bootstrap.css">
+		<script src="/js/jquery-2.1.3.js"></script>
+		<script src="/js/bootstrap.js"></script>	
 		<script>
 		$(document).ready(function () {
 				var statutUtil =  "<?php echo $_SESSION['statut']; ?>" ;
